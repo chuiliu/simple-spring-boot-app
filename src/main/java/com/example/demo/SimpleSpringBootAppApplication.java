1e0d573a44c196e137a83aa9febafa35c30986a5
chuiliu <zhengchuiliu@foxmail.com>
2018-03-09 11:01:31 +0800

    rename app

--- src/main/java/com/example/demo/SimpleSpringBootAppApplication.java
+++ src/main/java/com/example/demo/SimpleSpringBootAppApplication.java
@@ -6,9 +6,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class DemoApplication {
+public class SimpleSpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SimpleSpringBootAppApplication.class, args);
 	}
 }
